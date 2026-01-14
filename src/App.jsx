411ce5f0519f4837ab5efaaf85fba3158782a0e9
NSC <sumaiya.afroza.99@gmail.com>
2026-01-14 18:13:05 +0600

    cart details feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
 import Page from "./components/Page.jsx";
-import {MovieContext} from "./context/index.js";
+import {MovieContext, ThemeContext} from "./context/index.js";
 import {useReducer, useState} from "react";
 import {CartReducer , initialValue} from "./Reducers/CartReducer.js";
 import {ToastContainer} from "react-toastify";
 
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false)
+  const [darkMode, setDarkMode] = useState(() => {
+    const saveMode = localStorage.getItem('dark')
+    return saveMode === null ? true : saveMode === true
+  })
   const [state, dispatch ] = useReducer(CartReducer, initialValue)
 
   return (
     <>
-     <MovieContext.Provider value={{state, dispatch}}>
-       <Page/>
-       <ToastContainer
-         position={"bottom-right"}
-         autoClose={2000}
-       />
-     </MovieContext.Provider>
+      <ThemeContext value={{darkMode, setDarkMode}}>
+        <MovieContext.Provider value={{state, dispatch}}>
+          <Page/>
+          <ToastContainer
+            position={"bottom-right"}
+            autoClose={2000}
+            closeOnClick
+            theme={darkMode ? 'dark' : 'light'}
+          />
+        </MovieContext.Provider>
+      </ThemeContext>
+
     </>
   )
 }
